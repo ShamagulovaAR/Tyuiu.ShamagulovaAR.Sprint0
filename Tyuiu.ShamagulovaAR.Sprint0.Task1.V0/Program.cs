8cf7e042b107fee43555a054b14b6783e381cf5b
Tyuiu.ShamagulovaAR@outlook.com <Tyuiu.ShamagulovaAR@outlook.com>
2024-09-20 04:39:02 +0500

    Task1

---
+++ Tyuiu.ShamagulovaAR.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+namespace Tyuiu.ShamagulovaAR.Sprint0.Task0.V0
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "Ангелина";
+            int age = 20;
+            Console.WriteLine(" Привет! меня зовут " + name + " мне" + age + "лет!");
+            Console.ReadKey();
+        }
+    }
+}
