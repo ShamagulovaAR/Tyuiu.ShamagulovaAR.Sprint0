9ef46ae6eb303c3cfae55838065e6a0dfad9d6fb
Tyuiu.ShamagulovaAR@outlook.com <Tyuiu.ShamagulovaAR@outlook.com>
2024-09-20 06:49:59 +0500

    Task5

---
+++ Tyuiu.ShamagulovaAR.Sprint0.Task5.V0.Test/DataServiceTest.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.ShamagulovaAR.Sprint0.Task5.V0.Lib;
+
+namespace Tyuiu.ShamagulovaAR.Sprint0.Task5.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedAdditionValid()
+        {
+            Assert.AreEqual(10, DataService.Addition(5, 5));
+        }
+        public void CheckedSubtractionValid()
+        {
+            Assert.AreEqual(5, DataService.Subtraction(10, 5));
+        }
+        public void CheckedMultiplicationValid()
+        {
+            Assert.AreEqual(50, DataService.Multiplication(10, 5));
+        }
+        public void CheckedDivisionValid()
+        {
+            Assert.AreEqual(3, DataService.Division(9, 3));
+        }
+    }
+}
